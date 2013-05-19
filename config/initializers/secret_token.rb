ac8b00259ffb5cad4f69467c11125d1fcd82fc4f
captconrado <ottey001@gmail.com>
2013-05-19 14:33:42 -0700

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viet::Application.config.secret_token = '14a22ee43a3b02cf4e5100a72550e3e9cd9228d885eb3d62e3aa5cb3d3903ef6603e6864478b1f911d723e13a814167895b1cb706b848c3e2a2b653b40848228'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Viet::Application.config.session_store :cookie_store, key: '_viet_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Viet::Application.config.session_store :active_record_store
